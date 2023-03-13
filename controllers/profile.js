ff81e5540a101afc40ad0bee2430e16b9b486a46
amrinaas96 <amrinaas96@gmail.com>
2023-03-13 21:02:49 +0700

    Integrate clarifai API in backend

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,4 +1,7 @@
-const handleProfile = (req, res, database) => {
+import fetch from 'node-fetch';
+const env = process.env;
+
+export const handleProfile = (req, res, database) => {
     const { id } = req.params
     database.select('*').from('users').where({id}).then(user => {
         if (user.length) {
@@ -10,18 +13,64 @@ const handleProfile = (req, res, database) => {
     .catch(err => res.status(400).json('error getting user'))
 }
 
-const handleImage = (req, res, database) => {
-    const { id } = req.body
-    database('users').where('id', '=', id)
-    .increment('entries', 1)
-    .returning('entries')
-    .then(entries => {
-        res.json(entries[0].entries);
-    })
-    .catch(err => res.status(400).json('unable to get entries'))
-}
+export const handleImage = (req, res, database) => {
+    const { id, imageUrl } = req.body;
 
-module.exports = {
-    handleProfile: handleProfile,
-    handleImage: handleImage
+    // Your PAT (Personal Access Token) can be found in the portal under Authentification
+    const PAT = env['PAT_CLARIFAI'];
+    // Specify the correct user_id/app_id pairings
+    // Since you're making inferences outside your app's scope
+    const USER_ID = env['USER_ID_CLARIFAI'];       
+    const APP_ID = env['APP_ID_CLARIFAI'];
+
+    // Change these to whatever model and image URL you want to use
+    const MODEL_ID = env['MODEL_ID_CLARIFAI'];
+    const MODEL_VERSION_ID = env['MODEL_VERSION_ID_CLARIFAI'];    
+    const IMAGE_URL = imageUrl;
+
+    ///////////////////////////////////////////////////////////////////////////////////
+    // YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
+    ///////////////////////////////////////////////////////////////////////////////////
+    
+    const raw = JSON.stringify({
+        "user_app_id": {
+            "user_id": USER_ID,
+            "app_id": APP_ID
+        },
+        "inputs": [
+            {
+                "data": {
+                    "image": {
+                        "url": IMAGE_URL
+                    }
+                }
+            }
+        ]
+    });
+
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Key ' + PAT
+        },
+        body: raw
+    };
+
+    fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
+    .then(response => response.text())
+    .then(result => {
+        database('users').where('id', '=', id)
+        .increment('entries', 1)
+        .returning('entries')
+        .then(entries => {
+            res.json({
+                'result' : result,
+                'status': 200,
+                'entries': entries[0].entries
+            })
+        })
+        .catch(err => res.status(400).json('unable to get entries'))
+    })
+    .catch(error => console.log('error clarifai', error));
 }
